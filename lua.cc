4e97baeaadc992902b23af3b3278dbd6264dd9d0
Steve Kemp <steve@steve.org.uk>
2013-05-01 20:55:56 +0100

    Added Lua singleton.
      These two files implement a singleton-pattern, providing a global
     access to a Lua intepreter.

---
+++ lua.cc
@@ -0,0 +1,113 @@
+/**
+ * lua.cc - Singleton interface to an embedded Lua interpreter.
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <malloc.h>
+#include "lua.h"
+
+
+
+extern int my_function(lua_State * L);
+
+
+
+/**
+ * Instance-handle.
+ */
+CLua *CLua::pinstance = NULL;
+
+
+
+/**
+ * Get access to our LUA intepreter.
+ */
+CLua *CLua::Instance()
+{
+    if (!pinstance)
+	pinstance = new CLua;
+
+    return pinstance;
+}
+
+
+
+/**
+ * Constructor - This is private as this class is a singleton.
+ */
+CLua::CLua()
+{
+  /**
+   * Create LUA object.
+   */
+    m_lua = lua_open();
+    luaopen_base(m_lua);
+    luaL_openlibs(m_lua);
+
+    /**
+     * Version number
+     */
+    setGlobal("VERSION", "v0.2");
+
+    lua_register(m_lua, "my_function", my_function);
+}
+
+
+
+/**
+ * Load the specified lua file, and evaluate it.
+ */
+void CLua::loadFile(std::string filename)
+{
+    struct stat sb;
+
+    if ((stat(filename.c_str(), &sb) == 0))
+	if (0 == luaL_loadfile(m_lua, (char *) (filename.c_str())))
+	    lua_pcall(m_lua, 0, 0, 0);
+}
+
+
+/**
+ * Call a single Lua function, passing no arguments and ignoring the return code.
+ */
+bool CLua::callFunction(std::string name)
+{
+    lua_getglobal(m_lua, name.c_str());
+    if (lua_isfunction(m_lua, -1)) {
+        lua_pcall(m_lua, 0, 0, 0);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Set a global variable into the Lua environment.
+ */
+void CLua::setGlobal(std::string name, std::string value)
+{
+    lua_pushstring(m_lua, value.c_str());
+    lua_setglobal(m_lua, name.c_str());
+}
+
+
+/**
+ * Get a global variable value from the Lua environment.
+ */
+std::string * CLua::getGlobal(std::string name)
+{
+    std::string * result = NULL;
+
+    lua_getglobal(m_lua, name.c_str());
+    if (!lua_isnil(m_lua, -1))
+	result = new std::string(lua_tostring(m_lua, -1));
+
+    lua_pop(m_lua, 1);
+
+    return result;
+}
---
+++ lua.h
@@ -0,0 +1,73 @@
+/**
+ * lua.h - Singleton interface to an embedded Lua interpreter.
+ */
+
+
+#ifndef _clua_h__
+#define _clua_h_ 1
+
+extern "C" {
+   #include <lua.h>
+   #include <lauxlib.h>
+   #include <lualib.h>
+}
+
+
+/**
+ * A singleton class holding a Lua intepretter.
+ */
+class CLua
+{
+ public:
+
+  /**
+   * Get access to the singleton instance.
+   */
+  static CLua* Instance();
+
+  /**
+   * Load and execute a single file of Lua code.
+   */
+  void loadFile( std::string filename);
+
+  /**
+   * Call a single Lua function, passing no arguments and ignoring the return code.
+   */
+  bool callFunction( std::string name );
+
+  /**
+   * Set a global variable into the Lua environment.
+   */
+  void setGlobal( std::string name, std::string value );
+
+  /**
+   * Get a global variable value from the Lua environment.
+   */
+  std::string *getGlobal( std::string name );
+
+
+ protected:
+
+  /**
+   * Protected functions to allow our singleton implementation.
+   */
+  CLua();
+  CLua(const CLua&);
+  CLua& operator= (const CLua&);
+
+
+ private:
+
+  /**
+   * The single instance of this class.
+   */
+  static CLua* pinstance;
+
+  /**
+   * The handle to the lua intepreter.
+   */
+  lua_State *m_lua;
+
+};
+
+#endif /* _clua_h_ */
