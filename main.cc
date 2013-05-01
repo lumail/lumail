a8e810e86b6a595d4481e3a876bec8fa42f2d045
Steve Kemp <steve@steve.org.uk>
2013-05-01 16:36:43 +0100

    Stub file.
      This is roughly how things will work.

---
+++ main.cc
@@ -0,0 +1,24 @@
+
+//
+// int main( int argc, char *argv[] )
+// {
+//
+//   parse arguments
+//
+//   setup console
+//
+//   load lua environment
+//
+//   while( true )
+//        call: on_idle
+//        process events
+//        process keyboard
+//        refresh screen
+//   end
+//
+//   cleanup
+//
+// }
+
+
+
