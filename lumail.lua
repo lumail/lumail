03977429e6e2fcd57c999252905a94495bafbbcb
Steve Kemp <steve@steve.org.uk>
2013-05-04 14:52:24 +0100

    Defined functions for index/maildir/message-mode.
      These are helpers for when we get the keybindings setup.

--- lumail.lua
+++ lumail.lua
@@ -1,6 +1,3 @@
--- the first program in every language
-io.write("Hello world, from lua version ",_VERSION, " lumail version ", VERSION, "!\n")
-
 --
 -- Test getting/setting the current mode.
 --
@@ -14,8 +11,25 @@ end
 function on_exit()
    print("print: on_exit");
    io.write( "io.write: on_exit\n")
+   io.write("The global mode is: '" .. get_mode() .. "'.\n");
 end
 
 function on_idle()
    msg( "I am the lua function 'on_idle' - " .. os.date() .. "\n")
+end
+
+
+function index()
+   set_mode( "index" );
+   redraw_display();
+end
+
+function maildir()
+   set_mode( "maildir" );
+   redraw_display();
+end
+
+function message()
+   set_mode( "message" );
+   redraw_display();
 end
