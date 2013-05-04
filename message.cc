3539b33bcc24981679a41e2528e548c487079c85
Steve Kemp <steve@steve.org.uk>
2013-05-04 10:23:12 +0100

    Comment updates.

--- message.cc
+++ message.cc
@@ -1,18 +1,32 @@
+/**
+ * message.cc - A class for working with a single message.
+ */
+
+
 #include <string>
 #include "message.h"
 
+
+
 /**
- * A single message.
+ * Constructo.
  */
 CMessage::CMessage(std::string filename )
 {
   m_path = filename;
 }
 
+/**
+ * Destructor.
+ */
 CMessage::~CMessage()
 {
 }
 
+
+/**
+ * Get the path to the message.
+ */
 std::string CMessage::path()
 {
   return( m_path );
--- message.h
+++ message.h
@@ -1,3 +1,7 @@
+/**
+ * message.h - A class for working with a single message.
+ */
+
 #ifndef _message_h
 #define _message_h 1
 
@@ -10,12 +14,27 @@
 class CMessage
 {
  public:
+
+  /**
+   * Constructor
+   */
   CMessage(std::string filename);
+
+  /**
+   * Destructor.
+   */
   ~CMessage();
 
+  /**
+   * Get the path.
+   */
   std::string path();
 
  private:
+
+  /**
+   * The file we represent.
+   */
   std::string m_path;
 
 };
