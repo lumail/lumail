7d76603afe15e9f63fe343d882fb2c787244d56f
Steve Kemp <steve@steve.org.uk>
2013-05-07 23:51:07 +0100

    Added license (GPLv2+)
      About time I did this.  Also link to the domain-name.

--- bindings.cc
+++ bindings.cc
@@ -1,5 +1,20 @@
 /**
  * bindings.cc - Bindings for all functions callable from Lua.
+ *
+ * This file is part of lumail: http://lumail.org/
+ *
+ * Copyright (c) 2013 by Steve Kemp.  All rights reserved.
+ *
+ **
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 dated June, 1991, or (at your
+ * option) any later version.
+ *
+ * On Debian GNU/Linux systems, the complete text of version 2 of the GNU
+ * General Public License can be found in `/usr/share/common-licenses/GPL-2'
+ *
  */
 
 #include <stdio.h>
