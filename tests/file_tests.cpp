b1736f25157209d8f1906da1d424216868547b30
Steve Kemp <steve@steve.org.uk>
2013-06-01 23:04:50 +0100

    Added tests for CFile::move

--- tests/file_tests.cpp
+++ tests/file_tests.cpp
@@ -2,6 +2,7 @@
 
 #include "catch.hpp"
 #include "file.h"
+#include <sys/stat.h>
 
 
 TEST_CASE( "file/exists", "CFile::exists tests" )
@@ -70,4 +71,79 @@ TEST_CASE( "file/copy", "CFile::copy tests" )
     unlink( filename );
     REQUIRE_FALSE( CFile::exists( filename ) );
 
+}
+
+
+
+/**
+ * Generate two random files.
+ *
+ * Delete the second.
+ *
+ * Move the first one to the second.
+ *
+ * Ensure the first is gone, and that the second has the
+ * correct contents/
+ */
+TEST_CASE( "file/move", "CFile::move tests" )
+{
+    /**
+     * Generate a temporary file for our move-source.
+     * Generate a second temporary file for oure move-destination.
+     */
+    char file1[] = "/tmp/file.test.src.XXXXXX";
+    int fd1       = mkstemp(file1);
+
+    char file2[] = "/tmp/file.test.dst.XXXXXX";
+    int fd2       = mkstemp(file2);
+
+    REQUIRE( fd1 != -1 );
+    REQUIRE( fd2 != -1 );
+    close(fd1);
+    close(fd2);
+
+    /**
+     * Delete the second file, and ensure it is gone.
+     */
+    unlink( file2 );
+    REQUIRE( CFile::exists( file2 ) == false );
+
+    /**
+     * Open the first file to write known-good content to it.
+     */
+    FILE *f = fopen( file1, "w" );
+    fprintf(f, "This is a test\n" );
+    fclose( f );
+
+    /**
+     * Get the size of the first file.  Esnure it matches.
+     */
+    struct stat st;
+    REQUIRE( stat(file1, &st) == 0 );
+    REQUIRE( st.st_size == 15 );
+
+    /**
+     * Move the file now.
+     */
+    CFile::move( file1, file2 );
+
+    /**
+     * So the source should not exist, but the destination should.
+     */
+    REQUIRE( CFile::exists( file1 ) == false );
+    REQUIRE( CFile::exists( file2 ) == true );
+
+    /**
+     * The destination should also have the correct size.
+     */
+    REQUIRE( stat(file2, &st) == 0 );
+    REQUIRE( st.st_size == 15 );
+
+    /**
+     * Now cleanup both files.
+     */
+    unlink( file1 );
+    REQUIRE( CFile::exists( file1 ) == false );
+    unlink( file2 );
+    REQUIRE( CFile::exists( file2 ) == false );
 }
