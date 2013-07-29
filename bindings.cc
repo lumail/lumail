4f8b2d5744f02032b2cc5e32ab1127834cd55c29
Steve Kemp <steve@steve.org.uk>
2013-07-29 21:48:06 +0100

    Updated comment for the hook-function.

--- bindings.cc
+++ bindings.cc
@@ -133,8 +133,9 @@ CMessage *get_message_for_operation( const char *path )
 
 
 /**
- * Call the on_message_aborted hook, with the path to the
- * message.
+ * Call a hook, with the given path.
+ *
+ * For example "on_edit_message", "on_send_message", or "on_message_aborted".
  */
 void call_message_hook( const char *hook, const char *filename )
 {
