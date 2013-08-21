ece1000b29f11965f1afa49ed7964fd6bfe8654d
Steve Kemp <steve@steve.org.uk>
2013-08-21 08:52:39 +0100

    Added new test/demo-program.
        ./dump-parts ~/Maildir/.steve.org.uk/cur/1178920480.000011.mbox:2,RS
        File: /home/skx/Maildir/.steve.org.uk/cur/1178920480.000011.mbox:2,RS
        1 text/plain
        2 image/gif
        3 image/jpeg
        4 image/jpeg
    
      This is iterating over the body of the named message, and showing
     the multipart contents.
    
      We're intending to write three(??) primitives which will be similar
     to :
    
    * get_body_part( N ) - Returns the named part.
    * get_body_parts() - Which returns a table similar to the output above.
    * has_body_part( "text/plain" ) - Returns a boolean.
    
      Somehow we'll wire things up so that a function can return text
     for body display.
    
      This is part of 114.

---
+++ util/dump-parts.c
@@ -0,0 +1,91 @@
+
+#include <string>
+#include <string.h>
+#include <fcntl.h>
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <gmime/gmime.h>
+
+
+
+/**
+ * Dump the first text/plain part of a MIME-encoded message.
+ *
+ * If no such part is found try to dump the message-body regardless.
+ */
+void dump_mail( char *filename )
+{
+    std::string result;
+
+    GMimeMessage *m_message;
+    GMimeParser *parser;
+    GMimeStream *stream;
+    int fd;
+
+    std::cout << "File: " << filename << std::endl;
+
+    if ((fd = open( filename, O_RDONLY, 0)) == -1)
+    {
+        std::cout << "Opening failed ..." << std::endl;
+        return;
+    }
+
+    stream = g_mime_stream_fs_new (fd);
+
+    parser = g_mime_parser_new_with_stream (stream);
+    g_object_unref (stream);
+
+    m_message = g_mime_parser_construct_message (parser);
+    g_object_unref (parser);
+
+    /**
+     * Create an iterator
+     */
+    GMimePartIter *iter =  g_mime_part_iter_new ((GMimeObject *) m_message);
+    int count = 1;
+
+    /**
+     * Iterate over the message.
+     */
+    do
+    {
+        GMimeObject *part  = g_mime_part_iter_get_current (iter);
+
+        if ( ( GMIME_IS_OBJECT( part ) ) &&
+             ( GMIME_IS_PART(part) ) )
+        {
+            /**
+             * Get the content-type
+             */
+            GMimeContentType *content_type = g_mime_object_get_content_type (part);
+            gchar *type = g_mime_content_type_to_string ( content_type );
+            std::cout << count << " " << type << std::endl;
+        }
+        count += 1;
+    }
+    while (g_mime_part_iter_next (iter));
+
+    g_mime_part_iter_free (iter);
+    g_object_unref(m_message);
+}
+
+
+/**
+ * For each argument, do the dumping.
+ */
+int main( int argc, char *argv[] )
+{
+
+    g_mime_init(0);
+    for( int i = 1; i <argc; i++ )
+    {
+        dump_mail( argv[i] );
+    }
+    g_mime_shutdown();
+
+    return ( 0 );
+
+}
