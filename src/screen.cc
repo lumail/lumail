29376b4ce7047a5c4775bf51dc6f8576acaf9eb5
Steve Kemp <steve@steve.org.uk>
2013-10-26 23:57:55 +0100

    BUGFIX: clear() now forcibly refreshes the display.

--- src/screen.cc
+++ src/screen.cc
@@ -882,7 +882,12 @@ void CScreen::clear_main()
     for(int i = 0; i < ( height - 1 ); i++ )
         mvprintw( i, 0, "%s", blank.c_str() );
 
+    /**
+     * Ensure we refresh the display.
+     */
+    refresh();
 }
+
 
 /**
  * Choose a single item from a small selection.
