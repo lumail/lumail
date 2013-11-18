727159ae15971f45642bbc04031991362e65d556
Raphaël Rigo <devel-git@syscall.eu>
2013-11-18 23:47:00 +0100

    First try at parsing format specifiers correctly. (#161) We use pcrecpp to parse format strings, the sample files validates two examples. No color support yet.

--- util/parse-fmt.cc
+++ util/parse-fmt.cc
@@ -1,36 +1,86 @@
 #include <string>
 #include <string.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
+#include <pcrecpp.h>
 #include <cassert>
+#include <unordered_map>
 
 
+std::string get_field(const std::string spec, const std::string fieldname)
+{
+    const int fld_len = fieldname.length()+1;
+    int fldpos;
+    std::string res;
+
+    fldpos = spec.find(fieldname+":");
+
+    /** Make sure we have a real field definition */
+    if ( fldpos != std::string::npos && fldpos+fld_len < spec.length() )
+    {
+        int fldend;
+
+        /** skip "field:" */
+        fldend = fldpos + fld_len;
+
+        while ( fldend < spec.length() && isdigit(spec[fldend++]) );
+        if (fldend != spec.length() ) fldend--;
+
+        res = spec.substr(fldpos+fld_len, fldend-fldpos-fld_len);
+    }
+
+    return res;
+}
 /**
  * Expand a string such as '$SUBJECT{min:10 max:20 color:red} - Moi"
  * into something suitable.
  */
-std::string expand(std::string input , std::string subject)
+std::string expand_var(std::string input , std::unordered_map<std::string,std::string> headers)
 {
     /**
      * Split into the token and the optional arguments.
      */
+    pcrecpp::RE_Options options;
+    std::string var, beg, spec, result;
+    options.set_utf8(true);
+
+    pcrecpp::RE vars( "(.*?)\\$([a-zA-Z]+)\\{(.*?)\\}", options);
+
+    while( vars.PartialMatch(input, &beg, &var, &spec) )
+    {
+        int min = 0;
+        int max = -1;
+        const size_t match_len = 1 + var.length() + 1 + spec.length() + 1;
+        
+        result += beg;
+        input.erase(0, beg.length()+match_len);
+
+        min = atoi(get_field(spec, "min").c_str());
+        max = atoi(get_field(spec, "max").c_str());
+
+        std::string value = headers[var];
+
+        std::cout << var << " " << spec << "\n";
+        std::cout << min << " " << max << "\n";
+
+        if (min > max)
+            min = max;
+
+        if (min > 0 and min > value.length() )
+        {
+            result += value + std::string(min-value.length(), ' ');
+        } else if ( max < value.length() )
+            {
+                result += value.substr(0, max);
+            }
+    }
 
 
-    /**
-     * Convert "$SUBJECT" to the specified subject.
-     */
-
-    /**
-     * If min > 0 .. pad if necessary.
-     */
-
-    /**
-     * If max > 0 truncate.
-     */
-
-    return( input );
+    std::cout << ":" << result << ":\n";
+    return( result );
 }
 
 
@@ -41,8 +91,8 @@ std::string expand(std::string input , std::string subject)
 int main( int argc, char *argv[] )
 {
 
-    assert( expand( "$SUBJECT{min:10}", "steve" ) == "steve     " );
-    assert( expand( "$SUBJECT{max:3}", "12345" ) == "123" );
+    assert( expand_var( " $SUBJECT{min:10 max:20}", {{"SUBJECT","steve"}} ) == " steve     " );
+    assert( expand_var( "$SUBJECT{max:3}", {{"SUBJECT", "12345"}} ) == "123" );
 
     return 0;
 }
