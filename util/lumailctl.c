fa9113b5f2d4842f8dceef3d8f7ff184483a50a3
Steve Kemp <steve@steve.org.uk>
2014-03-18 08:34:49 +0000

    Allow socket to be default.

--- util/lumailctl.c
+++ util/lumailctl.c
@@ -10,6 +10,7 @@
  * --
  */
 
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdio.h>
@@ -25,14 +26,40 @@ int main(int argc, char *argv[])
   struct sockaddr_un addr;
   int fd,rc;
 
-  if ( argc != 3 )
+  char tmp[1024];
+  char *soc;
+  char *lua;
+
+  if ( argc == 2 )
   {
-      fprintf(stderr,"Usage: %s /path/to/socket 'cmd'\n", argv[0]);
+      memset( tmp, '\0', sizeof(tmp));
+      snprintf( tmp, sizeof(tmp)-1, "%s/.lumail.sock", getenv( "HOME" ) );
+
+      soc =  tmp;
+      lua = argv[1];
+  }
+  else if ( argc == 3 )
+  {
+      soc = argv[1];
+      lua = argv[2];
+  }
+  else
+  {
+      fprintf(stderr,"Usage: %s [socket/path] 'lua'\n", argv[0]);
       exit(0);
   }
 
-  char *socket_path = argv[1];
-  char *lua         = argv[2];
+
+  /**
+   * Ensure the socket exists.
+   */
+  struct stat sb;
+  if ( -1 == stat(soc, &sb) )
+  {
+      perror( "Socket not found.");
+      exit(-1);
+  }
+
 
   if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
   {
@@ -42,7 +69,7 @@ int main(int argc, char *argv[])
 
   memset(&addr, 0, sizeof(addr));
   addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+  strncpy(addr.sun_path, soc, sizeof(addr.sun_path)-1);
 
   if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
   {
