bd290f309cdc4506d3e1fa7d94c172d88f192c57
Steve Kemp <steve@steve.org.uk>
2015-10-24 11:52:07 +0300

    Added CLua singleton.
    This will be used to call Lua from C++, etc.
    
    This closes #4.

---
+++ src/lua.cc
@@ -0,0 +1,91 @@
+
+#include <cstdlib>
+
+#include "lua.h"
+
+
+/**
+ * External functions implemented in *_lua.cc
+ */
+extern void InitConfig (lua_State * l);
+extern void InitMaildir (lua_State * l);
+extern void InitMessage (lua_State * l);
+extern void InitMessagePart (lua_State * l);
+extern void InitScreen (lua_State * l);
+
+
+
+
+/**
+ * Get access to this singleton object.
+ */
+CLua *CLua::Instance()
+{
+    static CLua *instance = new CLua ();
+    return (instance);
+}
+
+
+
+/**
+ * Constructor - This is private as this class is a singleton.
+ */
+CLua::CLua()
+{
+    /**
+     * Create a new Lua object.
+     */
+    m_lua = luaL_newstate();
+
+
+    /**
+     * Register the libraries.
+     */
+    luaopen_base(m_lua);
+    luaL_openlibs(m_lua);
+
+
+    /**
+     * Load our bindings.
+     */
+    InitConfig (m_lua);
+    InitMaildir (m_lua);
+    InitMessage (m_lua);
+    InitMessagePart (m_lua);
+    InitScreen (m_lua);
+}
+
+/**
+ * Destructor.
+ */
+CLua::~CLua()
+{
+  lua_close (m_lua);
+
+}
+
+
+/**
+ * Load the specified Lua file, and evaluate it.
+ *
+ * Return true on success.  False on error.
+ */
+bool CLua::load_file(std::string filename)
+{
+  int erred = luaL_dofile (m_lua, filename.c_str ());
+  if (erred)
+    return false;
+  else
+    return true;
+}
+
+
+/**
+ * Evaluate the given string.
+ *
+ * Return true on success.  False on error.
+ */
+bool CLua::execute(std::string lua)
+{
+  return false;
+}
---
+++ src/lua.h
@@ -0,0 +1,55 @@
+
+#ifndef _LUA_H_
+#define _LUA_H_ 1
+
+extern "C"
+{
+# include <lua.h>
+# include <lauxlib.h>
+# include <lualib.h>
+}
+
+#include <string>
+
+
+/**
+ * A singleton class holding a Lua interpreter.
+ *
+ */
+class CLua
+{
+  private:
+    CLua ();
+    ~CLua ();
+
+ public:
+
+    /**
+     * Get access to this singleton instance.
+     */
+    static CLua *Instance();
+
+    /**
+     * Load the specified Lua file, and evaluate it.
+     *
+     * Return true on success.  False on error.
+     */
+    bool load_file(std::string filename);
+
+    /**
+     * Evaluate the given string.
+     *
+     * Return true on success.  False on error.
+     */
+    bool execute(std::string lua );
+
+private:
+
+    /**
+     * The handle to the Lua interpreter.
+     */
+    lua_State *m_lua;
+
+};
+
+#endif
