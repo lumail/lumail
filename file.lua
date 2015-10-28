0f93095d8a493885864fd59293f6837f062b8874
Steve Kemp <steve@steve.org.uk>
2015-10-28 21:38:30 +0200

    Added File:stat() method.
    This updates #17.

--- file.lua
+++ file.lua
@@ -1,6 +1,19 @@
 
-if File:exists( "/etc/passwd" ) then
-   print( "/etc/passwd exists." )
-else
-   print( "/etc/passwd does not exist." )
+function show_file( filename )
+   if ( File:exists( filename ) ) then
+      print( "File exists: " .. filename )
+
+      data = File:stat( filename )
+      for k,v in pairs( data ) do
+         print( "\t" .. k .. ": " .. v )
+      end
+   else
+      print( "File not found: " .. filename )
+   end
 end
+
+--
+-- Show some files
+--
+show_file( "/etc/passwd" )
+show_file( "/bin/ls" )
--- src/file_lua.cc
+++ src/file_lua.cc
@@ -24,11 +24,13 @@ extern "C"
 #include <lualib.h>
 }
 
+#include <sys/stat.h>
+
 #include "file.h"
 
 
 /**
- * Return the height of the panel.
+ * Does the given file exist?
  */
 int l_CFile_exists(lua_State * l)
 {
@@ -49,6 +51,83 @@ int l_CFile_exists(lua_State * l)
 
 
 /**
+ * Get details about the given file.
+ */
+int l_CFile_stat(lua_State * l)
+{
+    const char *str = lua_tostring(l, 2);
+    struct stat sb;
+
+    if (str == NULL)
+    {
+        return 0;
+    }
+
+    /**
+     * If we fail to stat the entry return nil.
+     */
+    if ((stat(str, &sb) != 0))
+    {
+        lua_pushnil(l);
+        return 1;
+    }
+
+
+    /**
+     * Convert the mode of the file to a string.
+     */
+    char mode[8];
+    snprintf(mode, 8, "0%o", sb.st_mode);
+
+
+    /**
+     * Push a table onto the stack, and return it.
+     */
+    lua_newtable(l);
+
+    lua_pushstring(l, "size");
+    lua_pushinteger(l, sb.st_size);
+    lua_settable(l, -3);
+
+    lua_pushstring(l, "uid");
+    lua_pushinteger(l, sb.st_uid);
+    lua_settable(l, -3);
+
+    lua_pushstring(l, "gid");
+    lua_pushinteger(l, sb.st_gid);
+    lua_settable(l, -3);
+
+    lua_pushstring(l, "mode");
+    lua_pushstring(l, mode + 3);
+    lua_settable(l, -3);
+
+    lua_pushstring(l, "type");
+
+    if (S_ISREG(sb.st_mode))
+        lua_pushstring(l, "file");
+    else if (S_ISDIR(sb.st_mode))
+        lua_pushstring(l, "directory");
+    else if (S_ISBLK(sb.st_mode))
+        lua_pushstring(l, "block");
+    else if (S_ISCHR(sb.st_mode))
+        lua_pushstring(l, "char");
+    else if (S_ISFIFO(sb.st_mode))
+        lua_pushstring(l, "fifo");
+    else if (S_ISLNK(sb.st_mode))
+        lua_pushstring(l, "link");
+    else if (S_ISSOCK(sb.st_mode))
+        lua_pushstring(l, "socket");
+    else
+        lua_pushstring(l, "unknown");
+
+    lua_settable(l, -3);
+
+
+    return 1;
+}
+
+
+/**
  * Export the `Class` class to Lua.
  *
  * Bind the appropriate methods to that object.
@@ -58,6 +137,7 @@ void InitFile(lua_State * l)
     luaL_Reg sFooRegs[] =
     {
         {"exists",  l_CFile_exists},
+        {"stat",    l_CFile_stat},
         {NULL,      NULL}
     };
     luaL_newmetatable(l, "luaL_CFile");
