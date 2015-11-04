0566efec2d3546a6befd0dcbad1b92f76958604a
Steve Kemp <steve@steve.org.uk>
2015-11-04 15:21:58 +0200

    Added command-line argument example.

---
+++ args.lua
@@ -0,0 +1,24 @@
+--
+--
+-- Usage:
+--
+--     ./lumail2 --load-file ./args.lua --no-curses --foo --bar
+--
+--
+
+
+--
+-- Process all arguments
+--
+for i,v in ipairs(ARGS) do
+
+    -- Show it
+    print( "Argument " .. i .. " is " .. v )
+
+
+    -- Sample handling of a real Lua-used-flag
+    folder = string.match(v, "--folder=(.*)" )
+    if ( folder ) then
+        print( "Should open folder " .. folder )
+    end
+end
