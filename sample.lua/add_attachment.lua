b90f0c8d1c0165dd3f17a70c372bd57648cdc331
Steve Kemp <steve@steve.org.uk>
2015-11-30 20:00:41 +0200

    Add sample for adding attachment to flat mail.
    This sample shows how things can/do work.
    
    This updates #83.

---
+++ sample.lua/add_attachment.lua
@@ -0,0 +1,51 @@
+--
+--
+-- Usage:
+--
+--     lumail2 --load-file ./add_attachment.lua --no-curses
+--
+--
+
+
+--
+-- Create a new message-file on-disk
+--
+local path = "msg.txt"
+local file = assert(io.open(path, "w"))
+
+--
+-- The content we'll write
+--
+header =[[To:Steve Kemp<steve@example.com>
+From: Steve Kemp <steve@example.net>
+Subject: This is a test
+
+
+I like cakes.
+
+Steve
+--
+]]
+
+--
+-- Write it out
+--
+file:write( header )
+file:close()
+
+
+--
+-- Now we'll create a Message object with that file
+--
+msg = Message.new(path)
+
+--
+-- Add an some attachments
+--
+local files = { "/etc/passwd", "/etc/motd" }
+msg:add_attachments( files )
+
+--
+-- Now we're done, show the result
+--
+os.execute( "less " .. msg:path() )
