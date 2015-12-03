f4ef7b21d6ce204883d864c22bec403914711123
Steve Kemp <steve@steve.org.uk>
2015-12-03 06:51:13 +0200

    Demonstrate MIME-type reading.

---
+++ sample.lua/mime_type.lua
@@ -0,0 +1,22 @@
+--
+--
+-- Usage:
+--
+--     lumail2 --load-file ./mime_type.lua --no-curses
+--
+--
+
+
+--
+-- For each file
+--
+local files = { "/etc/passwd", "/etc/motd" , "/bin/ls" }
+
+--
+-- Show the file + type
+--
+for i,o in ipairs(files) do
+   if ( File:exists( o ) ) then
+      print(string.format( "%64s - %32s", o, MIME.type(o)))
+   end
+end
