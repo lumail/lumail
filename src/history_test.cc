f02caf206ec5379bf688185435d0adc72e443a1c
Steve Kemp <steve@steve.org.uk>
2016-06-23 17:53:35 +0300

    Now we have CFile::size update our test.
    We can show persistence is working.  This updates #171.

--- src/history_test.cc
+++ src/history_test.cc
@@ -20,6 +20,7 @@
 
 #include <string.h>
 
+#include "file.h"
 #include "history.h"
 #include "CuTest.h"
 
@@ -75,11 +76,107 @@ void TestHistory(CuTest * tc)
 
 
 
-//
-// TODO: Implement CFile::size()
-//
-// Then test the history persistance.
-//
+/**
+ * Test CHistory persistence.
+ */
+void TestHistoryPersistence(CuTest * tc)
+{
+    /*
+     * Get the singleton
+     */
+    CHistory *instance = CHistory::instance();
+    CuAssertPtrNotNull(tc, instance);
+
+    /*
+     * History should be empty.
+     */
+    CuAssertIntEquals(tc, instance->size(), 0);
+
+    /**
+     * Create a file to save the history to.
+     */
+    char *t_src = strdup("srcXXXXXX");
+    char *src   = tmpnam(t_src);
+
+    /*
+     * The history file shouldn't exist, which means its size will be -1.
+     */
+    CuAssertTrue(tc, !CFile::exists(src));
+    CuAssertIntEquals(tc, CFile::size(src),-1);
+
+    /*
+     * Set the file, and write some strings.
+     */
+    instance->set_file( src );
+
+    /*
+     * Ensure the size increases
+     */
+    std::vector<std::string> tmp;
+    tmp.push_back("yksi");
+    tmp.push_back("kaski");
+    tmp.push_back("kolme");
+
+    int base = 0;
+
+    for (std::vector<std::string>::iterator it = tmp.begin(); it != tmp.end() ; ++it)
+    {
+        std::string txt = (*it);
+
+        /*
+         * Add one line of text.
+         */
+        instance->add(txt);
+
+
+        int size = CFile::size( src );
+
+        /*
+         * Test the size of the file is the existing size,
+         * plus the added-line, plus the newline.
+         */
+        CuAssertIntEquals(tc, base+txt.size()+1, size );
+
+        /*
+         * Bump our base-size - also add the newline.
+         */
+        base += txt.size();
+        base += 1;
+    }
+
+    /*
+     * Now change the filename, and we should find our history
+     * is empty.
+     */
+    instance->set_file( "bogus/path" );
+    CuAssertIntEquals(tc, 0,instance->size());
+
+    /*
+     * But if we go back to the file we read we should have
+     * our history, as expected.
+     */
+    instance->set_file( src );
+
+    /*
+     * Size should be three
+     */
+    CuAssertIntEquals(tc, 3, instance->size());
+
+    /*
+     * Entries should be in the right order.
+     */
+    for (int i = 0; i < (int)tmp.size(); i++)
+    {
+        CuAssertStrEquals(tc, tmp.at(i).c_str(), instance->at(i).c_str());
+    }
+
+    /*
+     * Remove our file
+     */
+    CFile::delete_file( src );
+    CuAssertTrue(tc, !CFile::exists(src));
+    CuAssertIntEquals(tc, CFile::size(src),-1);
+}
 
 
 CuSuite *
@@ -87,5 +184,6 @@ history_getsuite()
 {
     CuSuite *suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, TestHistory);
+    SUITE_ADD_TEST(suite, TestHistoryPersistence);
     return suite;
 }
