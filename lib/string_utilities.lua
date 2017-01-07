15eee483b07d76e6312656897038ef4d9ef5cdc5
Steve Kemp <steve@steve.org.uk>
2017-01-07 13:37:18 +0200

    Updated our string expansion routine.
    We've updated our string expansion routine specifically so that
    we can use it in formatting messages for the index.  That meant
    we needed to replace code like this:
    
              local output = string.format("[%4s] %2s - %-20s - %s%s",
                    flags, m_flags, sender, thread_indent, subject)
    
    i.e. We need to have size-bounds.
    
    So we can now write:
    
       print( string.interp( "Hello ${10|name}",
                            { name = "Apu Nahasapeemapetilon" } )
    
    This is part of #268.

--- lib/string_utilities.lua
+++ lib/string_utilities.lua
@@ -3,7 +3,7 @@
 -- Does the specified string end with the given string?
 --
 _G['string']['ends'] = function (String, End)
-  return End == '' or string.sub(String, -string.len(End)) == End
+   return End == '' or string.sub(String, -string.len(End)) == End
 end
 
 
@@ -16,24 +16,68 @@ end
 --
 --   print( string.interp( "Hello ${name}", { name = "World" } )
 --
+--
+-- String interopolation function, based upon code from the Lua wiki:
+--
+--   http://lua-users.org/wiki/StringInterpolation
+--
+-- Usage:
+--
+--   print( string.interp( "Hello ${name}", { name = "World" } )
+--
+-- As an extension we can limit the size of expanded strings:
+--
+--   print( string.interp( "Hello ${10|name}",
+--                        { name = "Apu Nahasapeemapetilon" } )
+--
+--  If the string is too long it will be truncted, and if too short will
+-- be (left-)padded with spaces.
+--
+--
 _G['string']['interp'] = function (s, tab)
-  return (s:gsub('($%b{})', function (w)
-      return tab[w:sub(3, -2)] or w
-    end))
+   return (s:gsub('($%b{})', function (key)
+                     -- Get the name of the table-key we'll lookup
+                     key = key:sub(3,-2)
+
+                     -- Does this key have a length restriction?
+                     local len,name = string.match(key , '^([0-9]+)|(.*)$' )
+
+                     -- If so
+                     if ( len and name ) then
+                        -- Get the value.
+                        local val = tab[name] or name
+
+                        -- Ensure our size is a number.
+                        len = tonumber(len)
+
+                        -- If the value is too long, truncate.
+                        if ( val:len() > len ) then
+                           val = val:sub(0,len)
+                        else
+                           -- Otherwise pad.
+                           while( val:len() < len ) do
+                              val = " " .. val
+                           end
+                        end
+                        return val
+                     else
+                        return tab[key] or key
+                     end
+                             end))
 end
 
 --
 -- Find the given binary on the $PATH, and return the full filename.
 --
 _G['string']['path'] = function (file)
-  local path = os.getenv "PATH"
-  local paths = string.split(path, ":;")
-  for i, o in pairs(paths) do
-    if File:exists(o .. "/" .. file) then
-      return (o .. "/" .. file)
-    end
-  end
-  return ""
+   local path = os.getenv "PATH"
+   local paths = string.split(path, ":;")
+   for i, o in pairs(paths) do
+      if File:exists(o .. "/" .. file) then
+         return (o .. "/" .. file)
+      end
+   end
+   return ""
 end
 
 
@@ -41,7 +85,7 @@ end
 -- Strip leading/trailing whitespace from the given string.
 --
 _G['string']['trim'] = function (s)
-  return string.match(s, '^()%s*$') and '' or string.match(s, '^%s*(.*%S)')
+   return string.match(s, '^()%s*$') and '' or string.match(s, '^%s*(.*%S)')
 end
 
 
@@ -51,13 +95,13 @@ end
 -- This is used in some drawing modes.
 --
 _G['string']['to_table'] = function (str)
-  local t = {}
-  local function helper (line)
-    table.insert(t, line)
-    return ""
-  end
-  helper((str:gsub("(.-)\r?\n", helper)))
-  return t
+   local t = {}
+   local function helper (line)
+      table.insert(t, line)
+      return ""
+   end
+   helper((str:gsub("(.-)\r?\n", helper)))
+   return t
 end
 
 
@@ -66,10 +110,10 @@ end
 -- table.
 --
 _G['string']['split'] = function (str, sep)
-  local result = {}
-  local regex = ("([^%s]+)"):format(sep)
-  for each in str:gmatch(regex) do
-    table.insert(result, each)
-  end
-  return result
+   local result = {}
+   local regex = ("([^%s]+)"):format(sep)
+   for each in str:gmatch(regex) do
+      table.insert(result, each)
+   end
+   return result
 end
