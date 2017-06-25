3ad4b4eac89b8b4d467bf46ccfec1882bb033c37
Steve Kemp <steve@steve.org.uk>
2017-06-25 09:57:48 +0300

    Disable testing of global.config.lua for the moment

--- t/test.loading.lua
+++ t/test.loading.lua
@@ -61,36 +61,6 @@ function Screen:exit ()
 end
 
 
---
--- Load our main configuration file.
---
--- This is actually the test - we're looking for references to
--- other libraries failing, syntax errors, etc.
---
-local loaded_lua = require "lumail2"
-
-
---
--- This is really a NOP - we're just pleased we could load
--- the library in the previous section
---
-function TestLumail2 ()
-  --
-  -- So we're running
-  --
-  luaunit.assertIsTrue(loaded_lua)
-
-  --
-  -- Test some functions exist.
-  --
-  luaunit.assertIsFunction(attachment_view)
-  luaunit.assertIsFunction(index_view)
-  luaunit.assertIsFunction(lua_view)
-  luaunit.assertIsFunction(lua_view)
-  luaunit.assertIsFunction(maildir_view)
-  luaunit.assertIsFunction(message_view)
-  luaunit.assertIsFunction(panel_view)
-end
 
 
 --
